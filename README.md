1df1a062b9ef11da8dc2e08da62c5d983f6f1739
Koji AGAWA <atty303@gmail.com>
2024-01-13 23:24:25 +0900

    feat: terms data

--- README.md
+++ README.md
@@ -15,7 +15,8 @@ A build planner for BB2 game.
    ```
 5. Run in C# Console.
    ```csharp
-   BGJson.Export(BGRepo.I, "path-to-db.json");
+   BGJson.Export(BGRepo.I, "dump\\db.json");
    ```
 6. Install [Asset Ripper](https://github.com/AssetRipper/AssetRipper).
-7. 
+7. Run Asset Ripper and select `File -> Open -> Open Folder`.
+8. Select `Export -> Export All` and export to `dump/asset`.
--- gen/src/main.rs
+++ gen/src/main.rs
@@ -1,3 +1,41 @@
+extern crate data;
+extern crate yaml_rust;
+
+use std::{fs, io};
+
+use yaml_rust::YamlLoader;
+
+use data::{LANGUAGES, Term, TermMap};
+
+fn write_terms() {
+    let s = fs::read_to_string("dump/asset/ExportedProject/Assets/Resources/I2Languages.asset").unwrap();
+    let docs = YamlLoader::load_from_str(s.as_str()).unwrap();
+    let doc = &docs[0];
+    let terms = doc["MonoBehaviour"]["mSource"]["mTerms"].as_vec().unwrap();
+
+    println!("{:?}", terms.len());
+
+    let mut outs = Vec::new();
+    for _lang in LANGUAGES {
+        outs.push(TermMap::new());
+    }
+
+    for term in terms {
+        let key = term["Term"].as_str().unwrap();
+        let langs = term["Languages"].as_vec().unwrap();
+        for (i, _lang) in LANGUAGES.iter().enumerate() {
+            let value = langs[2 + i].as_str().unwrap();
+            outs[i].insert(key.to_string(), Term { value: value.to_string() });
+        }
+    }
+
+    for (i, lang) in LANGUAGES.iter().enumerate() {
+        let file_writer = io::BufWriter::new(fs::File::create(format!("public/i18n/{}/terms.avro", lang)).unwrap());
+        TermMap::write(file_writer, &outs[i]).unwrap();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    write_terms();
+
 }
