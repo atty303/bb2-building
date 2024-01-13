d43a174db57d2964882106e82601636ae009d807
Koji AGAWA <atty303@gmail.com>
2024-01-14 00:38:59 +0900

    feat: tables csv

--- gen/src/main.rs
+++ gen/src/main.rs
@@ -1,41 +1,112 @@
+mod terms;
+
 extern crate data;
 extern crate yaml_rust;
+extern crate json;
+extern crate prettytable;
 
-use std::{fs, io};
+use std::collections::HashMap;
+use std::io::Write;
+use json::JsonValue;
+use prettytable::{Cell, Row};
 
-use yaml_rust::YamlLoader;
+fn main() {
+    terms::write_terms();
 
-use data::{LANGUAGES, Term, TermMap};
+    let s = std::fs::read_to_string("dump/db.json").unwrap();
+    let db_json = json::parse(s.as_str()).unwrap();
 
-fn write_terms() {
-    let s = fs::read_to_string("dump/asset/ExportedProject/Assets/Resources/I2Languages.asset").unwrap();
-    let docs = YamlLoader::load_from_str(s.as_str()).unwrap();
-    let doc = &docs[0];
-    let terms = doc["MonoBehaviour"]["mSource"]["mTerms"].as_vec().unwrap();
-
-    println!("{:?}", terms.len());
-
-    let mut outs = Vec::new();
-    for _lang in LANGUAGES {
-        outs.push(TermMap::new());
+    for meta in db_json["Metas"].members() {
+        let meta_name = meta["Name"].as_str().unwrap();
+        let table = Table::new(meta);
+        table.to_csv(std::io::BufWriter::new(std::fs::File::create(format!("dump/table/{}.csv", meta_name)).unwrap()));
     }
 
-    for term in terms {
-        let key = term["Term"].as_str().unwrap();
-        let langs = term["Languages"].as_vec().unwrap();
-        for (i, _lang) in LANGUAGES.iter().enumerate() {
-            let value = langs[2 + i].as_str().unwrap();
-            outs[i].insert(key.to_string(), Term { value: value.to_string() });
-        }
+    for meta in db_json["Metas"].members() {
+        let meta_name = meta["Name"].as_str().unwrap();
+        process_table(meta_name, meta);
     }
 
-    for (i, lang) in LANGUAGES.iter().enumerate() {
-        let file_writer = io::BufWriter::new(fs::File::create(format!("public/i18n/{}/terms.avro", lang)).unwrap());
-        TermMap::write(file_writer, &outs[i]).unwrap();
+}
+
+fn process_table(name: &str, meta: &JsonValue) {
+    let table = Table::new(meta);
+    match name {
+        _ => {
+            println!();
+            println!("unknown table: {:?}", name);
+            //table.print_entities();
+        },
     }
 }
 
-fn main() {
-    write_terms();
+struct Table<'a> {
+    db: &'a JsonValue,
+}
 
+impl Table<'_> {
+    fn new(db: &JsonValue) -> Table {
+        Table {
+            db,
+        }
+    }
+
+    fn verify(&self) {
+        assert_eq!(self.db["Keys"].len(), 1, "table should have one key");
+        assert_eq!(self.db["Keys"][0]["Name"].as_str().unwrap(), "key_ID", "table should have one key named 'id'");
+        assert_eq!(self.db["Keys"][0]["Unique"].as_bool().unwrap(), true, "table should have one key named 'id' and it should be unique");
+    }
+
+    fn fields(&self) -> Vec<String> {
+        let mut out = Vec::new();
+
+        for field in self.db["Fields"].members() {
+            out.push(field["Name"].as_str().unwrap().to_string());
+        }
+
+        out
+    }
+
+    fn entities(&self) -> Vec<HashMap<String, JsonValue>> {
+        let mut out = Vec::new();
+
+        for entity in self.db["Entities"].members() {
+            let mut entity_map = HashMap::new();
+
+            for field in entity["Values"].members() {
+                let field_name = field["Name"].as_str().unwrap();
+                let field_value = field["Value"].clone();
+
+                entity_map.insert(field_name.to_string(), field_value);
+            }
+
+            out.push(entity_map);
+        }
+
+        out
+    }
+
+    fn print_fields(&self) {
+        print!("fields: ");
+        for field in self.db["Fields"].members() {
+            print!("{}, ", field["Name"].as_str().unwrap());
+        }
+        println!();
+
+    }
+
+    fn to_csv<W: Write>(&self, writer: W) {
+        let mut table = prettytable::Table::new();
+        table.set_format(*prettytable::format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
+
+        let fs = self.fields();
+        table.set_titles(Row::new(fs.iter().map(|f| Cell::new(f)).collect()));
+
+        let es = self.entities();
+        for e in es.iter() {
+            table.add_row(Row::new(fs.iter().map(|f| Cell::new(format!("{}", e.get(f).unwrap()).as_str())).collect()));
+        }
+
+        table.to_csv(writer).unwrap();
+    }
 }
---
+++ gen/src/terms.rs
@@ -0,0 +1,31 @@
+use yaml_rust::YamlLoader;
+
+use data::{LANGUAGES, Term, TermMap};
+
+pub fn write_terms() {
+    let s = std::fs::read_to_string("dump/asset/ExportedProject/Assets/Resources/I2Languages.asset").unwrap();
+    let docs = YamlLoader::load_from_str(s.as_str()).unwrap();
+    let doc = &docs[0];
+    let terms = doc["MonoBehaviour"]["mSource"]["mTerms"].as_vec().unwrap();
+
+    println!("{:?}", terms.len());
+
+    let mut outs = Vec::new();
+    for _lang in LANGUAGES {
+        outs.push(TermMap::new());
+    }
+
+    for term in terms {
+        let key = term["Term"].as_str().unwrap();
+        let langs = term["Languages"].as_vec().unwrap();
+        for (i, _lang) in LANGUAGES.iter().enumerate() {
+            let value = langs[2 + i].as_str().unwrap();
+            outs[i].insert(key.to_string(), Term { value: value.to_string() });
+        }
+    }
+
+    for (i, lang) in LANGUAGES.iter().enumerate() {
+        let file_writer = std::io::BufWriter::new(std::fs::File::create(format!("public/i18n/{}/terms.avro", lang)).unwrap());
+        TermMap::write(file_writer, &outs[i]).unwrap();
+    }
+}
