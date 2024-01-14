8f2a098b050419be104cf413696f3cea73e66b54
Koji AGAWA <atty303@gmail.com>
2024-01-14 18:20:32 +0900

    feat: layout

--- src/components/skill_view.rs
+++ src/components/skill_view.rs
@@ -10,7 +10,7 @@ pub fn SkillView<'a>(cx: Scope<'a>, skill: &'a data::skill::Skill) -> Element {
     let database = use_shared_state::<Database>(cx).unwrap().read();
     render! {
         div {
-            class: "flex flex-col",
+            class: "flex flex-col bg-base-300 text-base-content rounded-md p-2 my-2",
             div {
                 class: "flex flex-row items-center gap-2",
                 Sprite { sprite: &skill.modes[0].icon }
@@ -20,7 +20,7 @@ pub fn SkillView<'a>(cx: Scope<'a>, skill: &'a data::skill::Skill) -> Element {
                 }
             }
             ul {
-                class: "flex flex-row gap-2",
+                class: "flex flex-row gap-2 p-2",
                 for mode in &skill.modes {
                     li {
                         SkillMode { mode: mode }
@@ -37,7 +37,7 @@ pub fn SkillMode<'a>(cx: Scope<'a>, mode: &'a data::skill::SkillMode) -> Element
 
     render! {
         div {
-            class: "flex flex-col gap-2 bg-neutral text-neutral-content rounded-md p-2",
+            class: "flex flex-col gap-2 bg-base-200 text-base-content rounded-md p-2",
             div {
                 class: "flex flex-row items-center gap-2",
                 Sprite { sprite: &mode.icon }
