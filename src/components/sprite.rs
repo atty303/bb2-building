92eb5af2dde73ac972f1516a2b2383e41115d60c
Koji AGAWA <atty303@gmail.com>
2024-01-14 18:47:09 +0900

    refactor: sprite

--- src/components/sprite.rs
+++ src/components/sprite.rs
@@ -2,19 +2,11 @@ use dioxus::prelude::*;
 
 #[component]
 pub fn Sprite<'a>(cx: Scope<'a>, sprite: &'a data::Sprite) -> Element {
-    let texture_height = 4096;
-    let x = sprite.x;
-    let y = sprite.y;
-    let width = sprite.width;
-    let height = sprite.height;
     render! {
         span {
-            class: "inline-block",
-            width: "{width}px",
-            min_width: "{width}px",
-            height: "{height}px",
-            min_height: "{height}px",
-            background: "url('image/a50549b8827f09843841d13f031f165f.png') -{x}px -{y}px",
+            class: "inline-block sprite",
+            style: "--x: {sprite.x}px; --y: {sprite.y}px; --w: {sprite.width}px; --h: {sprite.height}px; --s: 0.5"
         }
     }
+
 }
