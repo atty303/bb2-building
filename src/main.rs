931d824aa99445abe70542f4e249965c2866dfaa
Koji AGAWA <atty303@gmail.com>
2024-01-18 20:42:02 +0900

    feat: tracing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use tracing_subscriber::fmt::format::Pretty;
+use tracing_subscriber::prelude::*;
+use tracing_web::{performance_layer, MakeWebConsoleWriter};
+
 use crate::components::App;
 
 mod atoms;
@@ -6,6 +10,16 @@ mod hooks;
 mod pages;
 
 fn main() {
-    tracing_wasm::set_as_global_default();
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .with_ansi(false) // Only partially supported across browsers
+        .without_time() // std::time is not available in browsers, see note below
+        .with_writer(MakeWebConsoleWriter::new()); // write events to the console
+    let perf_layer = performance_layer().with_details_from_fields(Pretty::default());
+
+    tracing_subscriber::registry()
+        .with(fmt_layer)
+        .with(perf_layer)
+        .init(); // Install these as subscribers to tracing events
+
     dioxus_web::launch(App);
 }
