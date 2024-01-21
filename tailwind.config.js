a6126af8e1e2344b233f54955368973bf75b9491
Koji AGAWA <atty303@gmail.com>
2024-01-21 22:53:53 +0900

    chore: remove noto sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
     content: ["src/**/*.rs", "index.html"],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["Noto Sans JP", "sans-serif"],
-            },
-        },
-    },
     plugins: [require("@tailwindcss/typography"), require("daisyui")],
     daisyui: {
         themes: true,
