83b6b02806da322193a8a364f9bf6e0e926214d2
Koji AGAWA <atty303@gmail.com>
2024-01-23 20:20:29 +0900

    feat: planner

--- src/pages/planner/planner.rs
+++ src/pages/planner/planner.rs
@@ -1,15 +1,31 @@
-use crate::components::Icon;
+use crate::components::{Icon, SpriteIcon};
 use crate::hooks::{use_modal, ModalDialogProps};
 use crate::pages::skill::SkillList;
-use data::skill::Skill;
+use crate::pages::Route;
+use data::skill::{Skill, SkillHash};
+use data::RuneHash;
 use dioxus::prelude::*;
 use dioxus_signals::{use_signal, Signal};
+use fermi::use_read;
 use serde::{Deserialize, Serialize};
 use std::fmt::Display;
 use std::str::FromStr;
 
-#[derive(Default, PartialEq, Clone, Serialize, Deserialize)]
-pub struct PlannerState {}
+#[derive(Debug, Default, PartialEq, Clone, Serialize, Deserialize)]
+pub struct PlannerState {
+    build: BuildState,
+}
+
+#[derive(Debug, Default, PartialEq, Clone, Serialize, Deserialize)]
+struct BuildState {
+    slots: [SlotState; 5],
+}
+
+#[derive(Debug, Default, PartialEq, Clone, Serialize, Deserialize)]
+struct SlotState {
+    skill: Option<SkillHash>,
+    runes: [RuneHash; 5],
+}
 
 impl FromStr for PlannerState {
     type Err = serde_json::Error;
@@ -32,15 +48,27 @@ impl Display for PlannerState {
 pub fn PlannerPage(cx: Scope, state: PlannerState) -> Element {
     let skill_modal = use_modal(cx, "max-w-full h-full".to_string());
 
+    tracing::info!("state: {:?}", state);
+
     render! {
         div { class: "flex flex-col gap-4",
             for i in 0..5 {
                 PlannerSlot {
                     index: i,
-                    on_click: move |_| {
-                        skill_modal.show_modal(Signal::new(()), move |e| {
-                            tracing::debug!("Skill modal result: {:?}", e);
-                        });
+                    state: state.build.slots[i as usize].clone(),
+                    on_click: {
+                        let state = state.clone();
+                        move |_| {
+                            let state = state.clone();
+                            skill_modal.show_modal(Signal::new(()), move |e| {
+                                let mut state = state.clone();
+                                state.build.slots[i as usize].skill = Some(e);
+                                let router = dioxus_router::router();
+                                router.replace(Route::PlannerPage {
+                                    state: state,
+                                });
+                            });
+                        }
                     }
                 }
             }
@@ -50,13 +78,16 @@ pub fn PlannerPage(cx: Scope, state: PlannerState) -> Element {
     }
 }
 
-pub fn SkillModal<'a>(cx: Scope<'a, ModalDialogProps<'a, (), i32>>) -> Element {
+pub fn SkillModal<'a>(cx: Scope<'a, ModalDialogProps<'a, (), SkillHash>>) -> Element {
     let query = use_signal(cx, || "".to_string());
     let selected = use_signal(cx, || None::<Signal<Skill>>);
     render! {
         div { class: "sticky top-0 bg-base-300 p-2 z-10 mb-2",
             if let Some(skill) = *selected.read() {
                 button { class: "btn btn-primary",
+                    onclick: move |_| {
+                        cx.props.on_result.call(skill().hash);
+                    },
                     "Select {skill().name}"
                 }
             } else {
@@ -76,27 +107,49 @@ pub fn SkillModal<'a>(cx: Scope<'a, ModalDialogProps<'a, (), i32>>) -> Element {
 }
 
 #[component]
-pub fn PlannerSlot<'a>(cx: Scope<'a>, index: i32, on_click: EventHandler<'a, ()>) -> Element {
+pub fn PlannerSlot<'a>(
+    cx: Scope<'a>,
+    index: i32,
+    state: SlotState,
+    on_click: EventHandler<'a, ()>,
+) -> Element {
+    let db = use_read(cx, &crate::atoms::DATABASE);
+    let maybe_skill = state
+        .skill
+        .and_then(|hash| db.skill.get(&hash))
+        .map(|s| Signal::new(s.clone()));
+
     render! {
         div { class: "bg-base-300 rounded p-4",
-            onclick: move |_| on_click.call(()),
-            span { class: "hover:bg-primary border-primary border-solid border-2 rounded-md p-1 inline-block",
-                onclick: move |_| {
-
-                },
-                span { class: "relative",
-                    span { class: "inline-block align-middle overflow-hidden",
-                        width: "48px",
-                        height: "48px",
-                        line_height: "48px",
-                        Icon {
-                            class: "text-primary",
-                            svg: r#"<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-12"><path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>"#,
+            if let Some(skill) = maybe_skill {
+                span { class: "hover:bg-primary border-primary border-solid border-2 rounded-md p-1 inline-block",
+                    onclick: move |_| on_click.call(()),
+                    span { class: "relative",
+                        SpriteIcon { class: "rounded-md",
+                            sprite: Signal::new(skill.read().modes[0].icon.clone()),
+                            size: 48,
+                        }
+                    }
+                }
+                span {
+                    "{skill().name}"
+                }
+            } else {
+                span { class: "hover:bg-primary border-primary border-solid border-2 rounded-md p-1 inline-block",
+                    onclick: move |_| on_click.call(()),
+                    span { class: "relative",
+                        span { class: "inline-block align-middle overflow-hidden",
+                            width: "48px",
+                            height: "48px",
+                            line_height: "48px",
+                            Icon {
+                                class: "text-primary",
+                                svg: r#"<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-12"><path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>"#,
+                            }
                         }
                     }
                 }
             }
-
         }
     }
 }
